d8d8605b72c8f8329d63ae0a60a0c8c234445433
arunarvindh05 <arunarvindh05@gmail.com>
2022-10-16 21:35:22 +0530

    Adding all the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import CardList from './CardList';
+import data from './data.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {data.totalValue.map((value) => (
+            <div className="col-lg-4">
+              <div className="card mb-5 mb-lg-0">
+                <div className="card-body">
+                  <CardList details={value} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
---
+++ src/CardList.js
@@ -0,0 +1,46 @@
+export default function CardList({ details }) {
+  console.log(details);
+  return (
+    <div>
+      <h5 class="card-title text-muted text-uppercase text-center">
+        {details.name}
+      </h5>
+      <h6 class="card-price text-center">
+        ${details.price}
+        <span class="period">/month</span>
+      </h6>
+
+      <ul class="fa-ul">
+        {details.features.map((data, i) => (
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-check"></i>
+            </span>
+            {data !== "5 User" ? (
+              data !== "Unlimited User" ? (
+                data
+              ) : (
+                <strong>{data}</strong>
+              )
+            ) : (
+              <strong>{data}</strong>
+            )}
+          </li>
+        ))}
+        {details.nonFeatures.map((data, i) => (
+          <li className="text-muted">
+            <span className="fa-li">
+              <i className="fas fa-times"></i>
+            </span>
+            {data}
+          </li>
+        ))}
+      </ul>
+      <div class="d-grid">
+        <a href="#" className="btn btn-primary text-uppercase">
+          Button
+        </a>
+      </div>
+    </div>
+  );
+}
